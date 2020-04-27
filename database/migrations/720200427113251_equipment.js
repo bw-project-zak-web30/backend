9bbc3cd916c975ac5dbabaa3a556ee02c1243914
Michael-B1764 <mikebailar@gmail.com>
2020-04-27 12:21:27 -0700

    test

--- database/migrations/720200427113251_equipment.js
+++ database/migrations/720200427113251_equipment.js
@@ -7,6 +7,7 @@ exports.up = function(knex) {
           tbl.decimal('price').notNullable()
           tbl.integer('timeframe').notNullable()
           tbl.text('details')
+          tbl.boolean('renting').defaultTo(false)
           tbl.integer('owner_id')
               .unsigned()
               .notNullable()
@@ -14,7 +15,7 @@ exports.up = function(knex) {
               .inTable('users')
               .onUpdate('CASCADE')
               .onDelete('CASCADE')
-          tbl.boolean('renting').defaultTo(false)
+          
       })
   };
   
