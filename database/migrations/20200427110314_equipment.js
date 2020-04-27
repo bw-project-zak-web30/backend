fbe076e8e24b53da0e427224df01c761f3b6a11c
Michael-B1764 <mikebailar@gmail.com>
2020-04-27 11:07:48 -0700

    added migrations for equipment and rentals. removed the users migration for now.

---
+++ database/migrations/20200427110314_equipment.js
@@ -0,0 +1,24 @@
+
+exports.up = function(knex) {
+    return knex.schema
+      .createTable('equipment', tbl => {
+          tbl.increments();
+          tbl.text('name').notNullable()
+          tbl.decimal('price').notNullable()
+          tbl.integer('timeframe').notNullable()
+          tbl.text('details')
+          tbl.integer('owner-id')
+              .unsigned()
+              .notNullable()
+              .references('id')
+              .inTable('users')
+              .onUpdate('CASCADE')
+              .onDelete('CASCADE')
+          tbl.boolean('renting').defaultTo(false)
+      })
+  };
+  
+  exports.down = function(knex) {
+      return knex.schema.dropTableIfExists('equipment');
+  };
+  
