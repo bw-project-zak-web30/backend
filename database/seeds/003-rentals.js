2787bd5c0b581a65486678fd5a8023f43e17da2a
Michael-B1764 <mikebailar@gmail.com>
2020-04-27 14:50:10 -0700

    added some seed data

--- database/seeds/003-rentals.js
+++ database/seeds/003-rentals.js
@@ -1,9 +1,9 @@
 exports.seed = function(knex, Promise) {
     // Deletes ALL existing entries and resets ids
-    return knex('equipment')
+    return knex('rentals')
       .del()
       .then(function() {
-        return knex('equipment').insert([
+        return knex('rentals').insert([
           { user_id: 1, renter_id: 2, equipment_id: 2, return_date:  2020-10-10, start_date: 2020-10-8, details:"Rental is only for two days."},
         ]);
       });
